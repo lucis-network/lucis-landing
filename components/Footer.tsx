a23c1193cf6c90444809f332576f0b4cc768f1a2
Anderson William <1414377hcmut@gmail.com>
2022-01-21 19:05:44 +0800

    fix(footer): update ui footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,36 @@ type Props = {}
 export default function Header(props: Props) {
 
   return (
-   <h1>Footer</h1>
+   <section className={s.container}>
+     <div className={s.heading_footer}>
+      <div className={s.logo}>
+        <img src="/assets/logo.png" alt="" />
+      </div>
+      <div className={s.group_ic}>
+        <div className={s.ic_item}>
+          <img src="/assets/OurEcosystem/TikTok.png" alt="" />
+        </div>
+        <div className={s.ic_item}>
+          <img src="/assets/footer/fb.png" alt="" />
+        </div>
+        <div className={s.ic_item}>
+          <img src="/assets/footer/ytb.png" alt="" />
+        </div>
+        <div className={s.ic_item}>
+          <img src="/assets/OurEcosystem/tele.png" alt="" />
+        </div>
+        <div className={s.ic_item}>
+          <img src="/assets/footer/dis.png" alt="" />
+        </div>
+        <div className={s.ic_item}>
+          <img src="/assets/footer/Twitter.png" alt="" />
+        </div>
+      </div>
+     </div>
+
+    <div>
+      <img src="/assets/footer/footer.png" alt="" />
+    </div>
+   </section>
   )
 }

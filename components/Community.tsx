cd9005384e7492f79bab0d5d0aafb3fe9b6ba947
Anderson William <1414377hcmut@gmail.com>
2022-02-09 19:09:00 +0800

    fix(text): fix text game hot

--- components/Community.tsx
+++ components/Community.tsx
@@ -18,7 +18,7 @@ export default function Community(props: Props) {
       title: "AXIE INFINITY",
       genre: "Trading and Battle",
       content:
-        "Build up a collection and use them across an ever expanding universe of games! Axie Infinity uses cutting edge technology called Blockchain to reward players,...",
+        "Build up a collection and use them across an ever expanding universe of games! Axie Infinity uses cutting edge technology called Blockchain to reward players",
       src: "https://axieinfinity.com/",
     },
     {
@@ -26,7 +26,7 @@ export default function Community(props: Props) {
       title: "THETAN ARENA",
       genre: "MOBA E-Sport",
       content:
-        "Challenge yourself with a wide variety of game modes: MOBA & Battle Royale, coming with monthly updates and attractive rewards,...",
+        "Challenge yourself with a wide variety of game modes: MOBA & Battle Royale, coming with monthly updates and attractive rewards",
       src: "https://thetanarena.com",
     },
     {
@@ -34,7 +34,7 @@ export default function Community(props: Props) {
       title: "STAR SHARKS",
       genre: "Battle",
       content:
-        "StarSharks is a NFT-GameFi ecosystem based on the BSC chain developed by game players,...",
+        "StarSharks is a NFT-GameFi ecosystem based on the BSC chain developed by game players",
       src: "https://starsharks.com/",
     },
   ];

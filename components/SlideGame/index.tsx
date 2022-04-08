742a403c7af192f2501405b4d5b1f7e839c0120a
AndersonWilliam100 <thedv@lucis.network>
2022-04-08 15:18:57 +0800

    fix link slide game

--- components/SlideGame/index.tsx
+++ components/SlideGame/index.tsx
@@ -3,27 +3,25 @@ import { useWindowSize } from "hooks/useWindowSize";
 import React from "react";
 import Marquee from "react-fast-marquee";
 
-const img_game_green_beli = "/assets/slide_marquee/card1.png";
-const img_game_crypto_cars = "/assets/slide_marquee/card2.png";
-const img_game_howl_city = "/assets/slide_marquee/card3.png";
-const img_game_axie = "/assets/slide_marquee/card4.png";
-const img_game_theta_arena = "/assets/slide_marquee/card5.png";
-const img_game_kawaii_islands =
-  "/assets/slide_marquee/card6.png";
-const img_game_space = "/assets/slide_marquee/card7.png";
-const img_game_aspo = "/assets/slide_marquee/card8.png";
-const img_game_summoners_arena =
-  "/assets/slide_marquee/card9.png";
+const img_game_aurory = "/assets/slide_marquee/im_aurory.png";
+const img_game_big_time = "/assets/slide_marquee/im_big_time.png";
+const im_cyball = "/assets/slide_marquee/im_cyball.png";
+const im_illuvium = "/assets/slide_marquee/im_illuvium.png";
+const img_game_theta_arena = "/assets/slide_marquee/card1.png";
+const img_game_axie = "/assets/slide_marquee/card3.png";
+const im_delys = "/assets/slide_marquee/im_delys.png";
 
-const img_game_itam = "/assets/slide_marquee/card10.png";
-const img_game_warena = "/assets/slide_marquee/card11.png";
-const img_game_animverse = "/assets/slide_marquee/card12.png";
-const img_game_pegaxy = "/assets/slide_marquee/card13.png";
-const img_game_bomb_crypto = "/assets/slide_marquee/card14.png";
-const img_game_elemon = "/assets/slide_marquee/card15.png";
-const img_game_heroestd = "/assets/slide_marquee/card16.png";
-const img_game_star_sharks = "/assets/slide_marquee/card17.png";
-const img_game_death_road = "/assets/slide_marquee/card18.png";
+
+const of_kingdoms_arena = "/assets/slide_marquee/im_league of_kingdoms_arena.png";
+const im_meta_grear = "/assets/slide_marquee/im_meta_grear.png";
+const im_sandbox = "/assets/slide_marquee/im_sandbox.png";
+const im_sipher = "/assets/slide_marquee/im_sipher.png";
+const im_Star_Atlas = "/assets/slide_marquee/im_Star_Atlas.png";
+const im_step_n = "/assets/slide_marquee/im_step_n.png";
+const im_voxies = "/assets/slide_marquee/im_voxies.png";
+const img_game_animverse = "/assets/slide_marquee/card11.png";
+const img_game_star_sharks = "/assets/slide_marquee/card15.png";
+
 
 interface SlideItemProps {
   src: string;
@@ -58,20 +56,20 @@ export default function SlideGame() {
       <div style={{ width: "100%" }} className="section-content">
         <Marquee speed={90} gradient={false}>
           <SlideItem
-            addressGame='https://greenbeli.io/'
-            src={img_game_green_beli}
+            addressGame='https://aurory.io/'
+            src={img_game_aurory}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://cryptocars.me'
-            src={img_game_crypto_cars}
+            addressGame='https://bigtime.gg/'
+            src={img_game_big_time}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://www.howlcity.io/'
-            src={img_game_howl_city}
+            addressGame='https://cyball.com/'
+            src={im_cyball}
             width={widthImg}
             height={heightImg}
           />
@@ -87,39 +85,39 @@ export default function SlideGame() {
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://kawaii.global/'
-            src={img_game_kawaii_islands}
+            addressGame='https://play.illuivuim.io/'
+            src={im_illuvium}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://sip.space/' 
-            src={img_game_space} 
+            addressGame='https://www.delysium.com/' 
+            src={im_delys} 
             width={widthImg} 
             height={heightImg} />
           <SlideItem
-            addressGame='https://aspo.world/' 
-            src={img_game_aspo} 
+            addressGame='https://www.leagueofkingdoms.com/' 
+            src={of_kingdoms_arena} 
             width={widthImg} 
             height={heightImg} />
-          <SlideItem
-            addressGame='https://summonersarena.io/'
-            src={img_game_summoners_arena}
-            width={widthImg}
-            height={heightImg}
-          />
         </Marquee>
       </div>
       <div style={{ width: "100%", marginTop: "27px" }}>
         <Marquee speed={50} gradient={false} direction="right">
           <SlideItem
-            addressGame='https://itam.network/' 
-            src={img_game_itam} 
+            addressGame='https://metagear.game/'
+            src={im_meta_grear}
+            width={widthImg}
+            height={heightImg}
+          />
+          <SlideItem
+            addressGame='https://www.sandbox.game/en/' 
+            src={im_sandbox} 
             width={widthImg} 
             height={heightImg} />
           <SlideItem
-            addressGame='https://www.warena.io/'
-            src={img_game_warena}
+            addressGame='https://sipher.xyz/'
+            src={im_sipher}
             width={widthImg}
             height={heightImg}
           />
@@ -130,38 +128,26 @@ export default function SlideGame() {
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://pegaxy.io/'
-            src={img_game_pegaxy}
+            addressGame='https://wizardia.io/'
+            src={im_Star_Atlas}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://bombcrypto.io/'
-            src={img_game_bomb_crypto}
+            addressGame='https://stepn.com/'
+            src={im_step_n}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
-            addressGame='https://elemon.io/'
-            src={img_game_elemon}
-            width={widthImg}
-            height={heightImg}
-          />
-          <SlideItem
-            addressGame='https://dapp.heroestd.io'
-            src={img_game_heroestd}
+            addressGame='https://www.voxies.io/'
+            src={im_voxies}
             width={widthImg}
             height={heightImg}
           />
           <SlideItem
             addressGame='https://starsharks.com'
             src={img_game_star_sharks}
-            width={widthImg}
-            height={heightImg}
-          />
-          <SlideItem
-            addressGame='https://app.deathroad.io/'
-            src={img_game_death_road}
             width={widthImg}
             height={heightImg}
           />

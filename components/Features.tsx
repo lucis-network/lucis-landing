3c612485aafed6288572cb731ea5fb1f3ab35e44
The447 <1414377hcmut@gmail.com>
2022-05-28 10:34:38 +0700

    fix text ecosystem

--- components/Features.tsx
+++ components/Features.tsx
@@ -7,98 +7,98 @@ import ItemEcosystem from "./Home/Ecosystem/ItemEcosystem";
 
 type Props = {};
 const datas = [
-  {
-    id: 1,
-    titleH1: "Social-Fi network platform",
-    titleP: `
-        <br/>
-        <b>Social platform</b> <br/>
-        <p>A social network specialized for gamers in both NFT & traditional gamers. Highly customizable for every popular games<br/></p>
-        <br/>
-        <b>User Ranking system</b><br/>
-        <p>For team ranking & user ranking help them standing out and get more reputation, benefit from Lucis ecosystems.</p>
-    `,
-    titleButton: "More Detail",
-    href: "/social-fi",
-    srcImg: "/assets/Feature/social_fi1.png",
-    statusHref: true,
-  },
-  {
-    id: 2,
-    titleH1: "Tournaments",
-    titleP: `
-    <p>
-      <b>A platform</b> for creating, running tournament events.<br/>
-      <br/>
-      <b>Team</b> will get more popular and big prizes, big donation amount from Lucis Tournament platform
-    </p>
-    `,
-    titleButton: "More Detail",
-    href: "/tournaments",
-    srcImg: "/assets/Feature/tournaments1.png",
-    statusHref: true,
-  },
-  {
-    id: 3,
-    titleH1: "Lucis Insight & Game Ranking system",
-    titleP: `
-        <b>Lucis Insight</b> <br/>
-        <p>Our researcher team will carefully analysis the market, filter out hidden gems<br/></p>
-        <br/>
-        <b>Game Ranking System</b><br/>
-        <p>We auto collect game & market data and provide our users an completed overview and insight about a game with a glance.</p>
-    `,
-    titleButton: "More Detail",
-    href: "/insight",
-    srcImg: "/assets/Feature/insight1.png",
-    statusHref: true,
-  },
-  {
-    id: 4,
-    titleH1: "Lucis Media",
-    titleP: `
-        <b>lucis TV and Lucis blog</b>
-        <br />
-        <p>Will provide up to date and valuable info about the market, deeply analysis for game projects, NFT, and the whole markets</p>
-    `,
-    titleButton: "More Detail",
-    href: "/media",
-    srcImg: "/assets/Feature/media2.png",
-    statusHref: true,
-  },
-  {
-    id: 5,
-    titleH1: "Launchpad & Marketplace",
-    titleP: `
-      <p>
-        <b>Game publisher</b> can host INO to sell NFT mistery boxes to Lucis community or users<br/>
-        <br />
-        <b>Sell</b> NFT boxes or NFT collection on Lucis marketplace
-      </p>
-    `,
-    titleButton: "More Detail",
-    href: "/marketplace",
-    srcImg: "/assets/Feature/launchpad_marketplace.png",
-    statusHref: true,
-  },
-  {
-    id: 6,
-    titleH1: "Gaming Guild",
-    titleP: `
-      <p>We're starting from original Lucis Gaming Guild.</p>
-      <br>
-      <b>Gaming Guild tools:</b><br/>
-      <p>Our Lucis Guild Platform will connect investors to scholars</p>
-      <br/>
-      <b>Lucis Academy:</b>
-      <p>With our knowlegde, we're converting traditional gamers into Play 2 Earn gamers</p>
-      <p>We provide training and experience in Play 2 Earn field, to ensure our Scholars skill & quality</p>
-    `,
-    titleButton: "More Detail",
-    href: "/lucis-gaming-guild",
-    srcImg: "/assets/Feature/guild1.png",
-    statusHref: true,
-  },
+  // {
+  //   id: 1,
+  //   titleH1: "Social-Fi network platform",
+  //   titleP: `
+  //       <br/>
+  //       <b>Social platform</b> <br/>
+  //       <p>A social network specialized for gamers in both NFT & traditional gamers. Highly customizable for every popular games<br/></p>
+  //       <br/>
+  //       <b>User Ranking system</b><br/>
+  //       <p>For team ranking & user ranking help them standing out and get more reputation, benefit from Lucis ecosystems.</p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/social-fi",
+  //   srcImg: "/assets/Feature/social_fi1.png",
+  //   statusHref: true,
+  // },
+  // {
+  //   id: 2,
+  //   titleH1: "Tournaments",
+  //   titleP: `
+  //   <p>
+  //     <b>A platform</b> for creating, running tournament events.<br/>
+  //     <br/>
+  //     <b>Team</b> will get more popular and big prizes, big donation amount from Lucis Tournament platform
+  //   </p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/tournaments",
+  //   srcImg: "/assets/Feature/tournaments1.png",
+  //   statusHref: true,
+  // },
+  // {
+  //   id: 3,
+  //   titleH1: "Lucis Insight & Game Ranking system",
+  //   titleP: `
+  //       <b>Lucis Insight</b> <br/>
+  //       <p>Our researcher team will carefully analysis the market, filter out hidden gems<br/></p>
+  //       <br/>
+  //       <b>Game Ranking System</b><br/>
+  //       <p>We auto collect game & market data and provide our users an completed overview and insight about a game with a glance.</p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/insight",
+  //   srcImg: "/assets/Feature/insight1.png",
+  //   statusHref: true,
+  // },
+  // {
+  //   id: 4,
+  //   titleH1: "Lucis Media",
+  //   titleP: `
+  //       <b>lucis TV and Lucis blog</b>
+  //       <br />
+  //       <p>Will provide up to date and valuable info about the market, deeply analysis for game projects, NFT, and the whole markets</p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/media",
+  //   srcImg: "/assets/Feature/media2.png",
+  //   statusHref: true,
+  // },
+  // {
+  //   id: 5,
+  //   titleH1: "Launchpad & Marketplace",
+  //   titleP: `
+  //     <p>
+  //       <b>Game publisher</b> can host INO to sell NFT mistery boxes to Lucis community or users<br/>
+  //       <br />
+  //       <b>Sell</b> NFT boxes or NFT collection on Lucis marketplace
+  //     </p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/marketplace",
+  //   srcImg: "/assets/Feature/launchpad_marketplace.png",
+  //   statusHref: true,
+  // },
+  // {
+  //   id: 6,
+  //   titleH1: "Gaming Guild",
+  //   titleP: `
+  //     <p>We're starting from original Lucis Gaming Guild.</p>
+  //     <br>
+  //     <b>Gaming Guild tools:</b><br/>
+  //     <p>Our Lucis Guild Platform will connect investors to scholars</p>
+  //     <br/>
+  //     <b>Lucis Academy:</b>
+  //     <p>With our knowlegde, we're converting traditional gamers into Play 2 Earn gamers</p>
+  //     <p>We provide training and experience in Play 2 Earn field, to ensure our Scholars skill & quality</p>
+  //   `,
+  //   titleButton: "More Detail",
+  //   href: "/lucis-gaming-guild",
+  //   srcImg: "/assets/Feature/guild1.png",
+  //   statusHref: true,
+  // },
   {
     id: 7,
     titleH1: "Lucis Play-to-earn 2.0 (Zone 1)",
@@ -109,19 +109,7 @@ const datas = [
       <br />
       <b>Play-to-earn 2.0</b>
       <p>
-        Players can earn Lucis tokens, Lucis points and other valuable rewards simply by playing their favorite traditional games such as CS:GO, League of Legends, Valorant...
-      </p>
-      <p>
-        - It is completely free to begin earning rewards
-      </p>
-      <p>
-        - Rank up to earn more
-      </p>
-      <p>
-        - Points can be converted into Raffle tickets or Lucis tokens, and Lucis token reward earnings have monetary value
-      </p>
-      <p>
-        - NFT is a symbol of the player's personal status in Lucis's gaming SocialFi
+        <b>Players</b> can earn Lucis tokens, Lucis points and other valuable rewards simply by playing their favorite traditional games such as CS:GO, League of Legends, Valorant... 
       </p>
     `,
     titleButton: "Coming Soon",
@@ -141,12 +129,6 @@ const datas = [
       </p>
       <p>
         Players can create their own teams, clubs, and tournaments
-      </p>
-      <p>
-        Opportunity to form official partnerships gaming clubs, as well as professional esports leagues
-      </p>
-      <p>
-        Tournament organizers earn rewards in the same way that tournament winners do
       </p>
       <br />
       <b>
@@ -219,7 +201,7 @@ const datas = [
     `,
     titleButton: "Coming Soon",
     href: "",
-    srcImg: "/assets/Feature/zone5.png",
+    srcImg: "/assets/Feature/tournaments1.png",
     statusHref: true,
   },
   {
@@ -259,14 +241,14 @@ export default function Features(props: Props) {
 
   return (
     <section className={`${s.wrapper}`} id="EcoSystem">
-      <div className={`lucis-container`}>
+      <div className={`lucis-container ${s.container}`}>
         <TitleName titleName="Lucis Ecosystem" />
 
         <div className={s.BlockContent}>
           <ItemEcosystem dataEcosystems={datas} />
         </div>
 
-        <img src="/assets/Feature/im_diagram.png" alt="" />
+        {/* <img src="/assets/Feature/im_diagram.png" alt="" /> */}
       </div>
     </section>
   );

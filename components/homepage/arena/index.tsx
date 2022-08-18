0dd19be767e63d3b4d9c8b19a9704f377f43f282
ducvt <ducvt@lucis.network>
2022-08-18 14:21:45 +0700

    add BG and fix luckychest

--- components/homepage/arena/index.tsx
+++ components/homepage/arena/index.tsx
@@ -34,66 +34,68 @@ export default function Arena() {
 
   return (
     <>
-      <div className={s.wrapper}>
-        <div className={`lucis-container-2 ${s.container}`}>
-          <div className={s.title}>
-            <img src="/assets/homepage/arena.png" alt=""/>
-          </div>
-          <div className={s.listItems}>
-            <Swiper
-              slidesPerView={2}
-              spaceBetween={8}
-              initialSlide={1}
-              loop={true}
-              pagination={{
-                clickable: true
-              }}
-              breakpoints={{
-                360: {
-                  slidesPerView: 2,
-                  spaceBetween: 7.5
-                },
-                575: {
-                  slidesPerView: 3,
-                  spaceBetween: 8
-                },
-                768: {
-                  slidesPerView: 3,
-                  spaceBetween: 16
-                },
-                1320: {
-                  slidesPerView: 4,
-                  spaceBetween: 32
-                }
-              }}
-            >
-            {
-              data && data.map((item, index) => {
-                return (
-                  <div key={`${item?.team_size}-${item?.uid}`}>
-                    <SwiperSlide onClick={() => getDetailArena(item)} key={`${item?.team_size}-${item?.uid}`}>
-                      <ItemArena item={item}></ItemArena>
-                    </SwiperSlide>
-                  </div>
-                )
-              })
-            }
-            </Swiper>
-          </div>
-
-          <div className={s.desc}>
-            <p>Let's create your own playing field and show your level to others!</p>
-          </div>
-          <div className={s.groupBtn}>
-            <div className={`${s.btn} ${s.btnJoin}`} onClick={joinNow}>
-              <div>Join Now</div>
+      <section>
+        <div className={s.wrapper}>
+          <div className={`lucis-container-2 ${s.container}`}>
+            <div className={s.title}>
+              <img src="/assets/homepage/arena.png" alt=""/>
             </div>
-            <div className={`${s.btn} ${s.btnCreate}`} onClick={createNow}>
-              <div>Create Now</div>
+            <div className={s.listItems}>
+              <Swiper
+                slidesPerView={2}
+                spaceBetween={8}
+                initialSlide={1}
+                loop={true}
+                pagination={{
+                  clickable: true
+                }}
+                breakpoints={{
+                  360: {
+                    slidesPerView: 2,
+                    spaceBetween: 7.5
+                  },
+                  575: {
+                    slidesPerView: 3,
+                    spaceBetween: 8
+                  },
+                  768: {
+                    slidesPerView: 3,
+                    spaceBetween: 16
+                  },
+                  1320: {
+                    slidesPerView: 4,
+                    spaceBetween: 32
+                  }
+                }}
+              >
+                {
+                  data && data.map((item, index) => {
+                    return (
+                      <div key={`${item?.team_size}-${item?.uid}`}>
+                        <SwiperSlide onClick={() => getDetailArena(item)} key={`${item?.team_size}-${item?.uid}`}>
+                          <ItemArena item={item}></ItemArena>
+                        </SwiperSlide>
+                      </div>
+                    )
+                  })
+                }
+              </Swiper>
+            </div>
+
+            <div className={s.desc}>
+              <p>Let's create your own playing field and show your level to others!</p>
+            </div>
+            <div className={s.groupBtn}>
+              <div className={`${s.btn} ${s.btnJoin}`} onClick={joinNow}>
+                <div>Join Now</div>
+              </div>
+              <div className={`${s.btn} ${s.btnCreate}`} onClick={createNow}>
+                <div>Create Now</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
       </>
   );
 };
--- components/homepage/luckychest/index.tsx
+++ components/homepage/luckychest/index.tsx
@@ -26,79 +26,86 @@ export default function LuckyChest() {
 
   return (
     <>
-      <div className={s.wrapper}>
-        <div className={`lucis-container-2 ${s.container}`}>
-          <div >
-            <div className={s.listItems}>
-              {getChestDetailData?.prizes?.slice(0,16).map(prize => (
-                <>
-                  <ChestPrize
-                    key={prize?.id}
-                    description={prize?.desc}
-                    image={prize?.img ?? ''}
-                    title={prize?.title}
-                    rarity={prize?.rarity}
-                  />
-                </>
-              ))}
-              {/*<Swiper*/}
-              {/*  slidesPerView={8}*/}
-              {/*  spaceBetween={8}*/}
-              {/*  //initialSlide={1}*/}
-              {/*  // grid={{*/}
-              {/*  //   rows: 1,*/}
-              {/*  // }}*/}
-              {/*  pagination={{*/}
-              {/*    clickable: true*/}
-              {/*  }}*/}
-              {/*  //modules={[Grid]}*/}
-              {/*  // breakpoints={{*/}
-              {/*  //   360: {*/}
-              {/*  //     slidesPerView: 2,*/}
-              {/*  //     spaceBetween: 7.5*/}
-              {/*  //   },*/}
-              {/*  //   575: {*/}
-              {/*  //     slidesPerView: 3,*/}
-              {/*  //     spaceBetween: 8*/}
-              {/*  //   },*/}
-              {/*  //   768: {*/}
-              {/*  //     slidesPerView: 3,*/}
-              {/*  //     spaceBetween: 16*/}
-              {/*  //   },*/}
-              {/*  //   1320: {*/}
-              {/*  //     slidesPerView: 4,*/}
-              {/*  //     spaceBetween: 32*/}
-              {/*  //   }*/}
-              {/*  // }}*/}
-              {/*>*/}
-              {/*  {getChestDetailData?.prizes?.slice(0,16).map((prize,index) => (*/}
-              {/*    <>*/}
-              {/*      <div key={`${prize?.id}-${index}`}>*/}
-              {/*        <SwiperSlide  key={`${prize?.id}-${index}`}>*/}
-              {/*          <ChestPrize*/}
-              {/*            key={prize?.id}*/}
-              {/*            description={prize?.desc}*/}
-              {/*            image={prize?.img ?? ''}*/}
-              {/*            title={prize?.title}*/}
-              {/*            rarity={prize?.rarity}*/}
-              {/*          />*/}
-              {/*        </SwiperSlide>*/}
-              {/*      </div>*/}
+      <section>
+        {/*<div className={`lucis-container-2 ${s.container1}`}>*/}
+        {/*  <div className={s.rowData}>*/}
+        {/*    <span className={s.time}>June 29th, 6:30 am </span>*/}
+        {/*    <span className={s.name}>Ngô Thị Là Ánh Dương </span>*/}
+        {/*    <span className={s.desc}>Received a prize </span>*/}
+        {/*    <span className={s.value}>Valued at €20</span>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        <div className={s.wrapper}>
+          <div className={`lucis-container-2 ${s.container}`}>
+            <div className={s.leftContainer}>
+              <div className={s.listItems}>
+                {/*{getChestDetailData?.prizes?.slice(0,16).map(prize => (*/}
+                {/*  <>*/}
+                {/*    <ChestPrize*/}
+                {/*      key={prize?.id}*/}
+                {/*      description={prize?.desc}*/}
+                {/*      image={prize?.img ?? ''}*/}
+                {/*      title={prize?.title}*/}
+                {/*      rarity={prize?.rarity}*/}
+                {/*    />*/}
+                {/*  </>*/}
+                {/*))}*/}
+                <Swiper
+                  slidesPerView={4}
+                  //spaceBetween={8}
+                  //initialSlide={1}
+                  loop={true}
+                  // grid={{
+                  //   rows: 2,
+                  // }}
+                  pagination={{
+                    clickable: true
+                  }}
+                  modules={[Grid]}
+                  breakpoints={{
+                    360: {
+                      slidesPerView: 4,
+                      spaceBetween: 8
+                    },
+                    575: {
+                      slidesPerView: 6,
+                      spaceBetween: 8
+                    },
+                    768: {
+                      slidesPerView: 8,
+                      spaceBetween: 16
+                    },
+                  }}
+                >
+                  {getChestDetailData?.prizes?.slice(0,16).map((prize,index) => (
+                    <>
+                      <div key={`${prize?.id}-${index}`}>
+                        <SwiperSlide  key={`${prize?.id}-${index}`}>
+                          <ChestPrize
+                            key={prize?.id}
+                            description={prize?.desc}
+                            image={prize?.img ?? ''}
+                            title={prize?.title}
+                            rarity={prize?.rarity}
+                          />
+                        </SwiperSlide>
+                      </div>
 
-              {/*    </>*/}
-              {/*  ))}*/}
-              {/*</Swiper>*/}
+                    </>
+                  ))}
+                </Swiper>
+              </div>
+              <div className={`${s.btn}`} onClick={joinNow}>
+                <div>LUCKY CHEST</div>
+              </div>
             </div>
-            <div className={`${s.btn}`} onClick={joinNow}>
-              <div>LUCKY CHEST</div>
-            </div>
-          </div>
 
-          <div className={s.boxImages}>
-            <img src="/assets/homepage/box_luckychest.svg"  alt=""/>
+            <div className={s.boxImages}>
+              <img src="/assets/homepage/box_luckychest.svg"  alt=""/>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
       </>
   );
 };
